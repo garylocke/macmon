5a5c81e4560a65f7f71d8d0f78a942553b3f5794
Gary Locke <garylocke@Garys-MacBook-Pro.local>
2014-07-08 17:16:43 -0400

    Connects to json file on Nagios server. Data parsing logic started.

--- MacMon/AppDelegate.m
+++ MacMon/AppDelegate.m
@@ -10,9 +10,92 @@
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
-    // Insert code here to initialize your application
+@synthesize serverUrl;
+@synthesize username;
+@synthesize password;
+@synthesize output;
+
+NSURLConnection *urlConnection;
+
+NSString *serverUrlString;
+NSString *usernameString;
+NSString *passwordString;
+NSString *output;
+
+NSString *statusUrl;
+
+BOOL isConnected;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification{}
+
+- (IBAction)connect:(id)sender {
+    
+    serverUrlString = [NSString stringWithFormat:@"%@",[self.serverUrl stringValue]];
+    usernameString = [NSString stringWithFormat:@"%@",[self.username stringValue]];
+    passwordString = [NSString stringWithFormat:@"%@",[self.password stringValue]];
+    
+    statusUrl = [NSString stringWithFormat:@"%@/rss/rss.json",[self.serverUrl stringValue]];
+    
+    NSURL *url = [NSURL URLWithString:serverUrlString];
+    NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:url];
+    
+    urlConnection = [[NSURLConnection alloc] initWithRequest:urlRequest delegate:self];
+}
+
+- (IBAction)dataButton:(id)sender {
+    [self getServerStatusData];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge{
+    if ([challenge previousFailureCount] > 1)
+    {
+        NSLog(@"auth failed");
+    }
+    else
+    {
+        NSURLCredential *cred = [[NSURLCredential alloc] initWithUser:usernameString password:passwordString persistence:NSURLCredentialPersistenceForSession];
+        [[challenge sender] useCredential:cred forAuthenticationChallenge:challenge];
+    }
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection{
+    isConnected = true;
+    NSLog(@"Connected");
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error{
+    NSLog(@"Could not authenticate to server.");
+}
+
+- (void)getServerStatusData{
+    if(isConnected){
+        NSString *urlString = [NSString stringWithFormat:@"%@",statusUrl];
+        NSURL *url = [NSURL URLWithString:urlString];
+        
+        NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+        [[session dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error){
+            //NSLog(@"%@",response);
+            
+            //NSString *dataString = [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
+            //NSLog(@"%@",dataString);
+
+            NSError *err = nil;
+            NSDictionary *statusDictionary = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&err];
+
+//            NSDictionary *hosts = [statusDictionary objectForKey:@"hosts"];
+//            NSDictionary *firstHostValue = [hosts valueForKey:@"micro.synergeticmarketing.net"];
+//            NSString *hostName = [firstHostValue valueForKey:@"host_name"];
+
+            for(NSString *key in statusDictionary){
+                id value = [statusDictionary objectForKey:key];
+                if([value isKindOfClass:[NSDictionary class]]){
+                    NSLog(@"dictionary found");
+                } else if([value isKindOfClass:[NSString class]]){
+                    NSLog(@"string found");
+                }
+            }
+        }] resume];
+    }
 }
 
 @end
